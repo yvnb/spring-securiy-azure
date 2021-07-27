4d04b12c2f94570978ee683ea286fa418d10170e
yvnb <yvnagendrababu@gmail.com>
2021-07-27 13:15:09 +0400

    Add files via upload

---
+++ SpringSecurityAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure.springsecurityazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAzureApplication.class, args);
+	}
+
+}
